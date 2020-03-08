ac01ccc0180398ff498af82678ad67a21ae1c14d
Pulak Kumar Das <pulak1901@gmail.com>
2020-03-08 14:10:11 +0530

    Initial interpreter code

---
+++ Skell/Interpreter/Interpreter.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Interpreter
+{
+    class SkellInterpreter
+    {
+        public void interprete(string src)
+        {
+            Console.WriteLine(src);
+        }
+    }
+}
--- Skell/Program.cs
+++ Skell/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+
+using Interpreter;
 
 namespace Skell
 {
     class Program
     {
+        private static SkellInterpreter interpreter;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 1) {
+                Console.WriteLine("Usage: dotnet run -- [script]");
+                Environment.Exit(127);
+            } else {
+                interpreter = new SkellInterpreter();
+                if (args.Length == 1) {
+                    runFile(args[0]);
+                } else {
+                    runPrompt();
+                }
+            }
+        }
+
+        public static void runFile(String path)
+        {
+            byte[] input = File.ReadAllBytes(path);
+            interpreter.interprete(Encoding.ASCII.GetString(input));
+        }
+
+        public static void runPrompt()
+        {
+            Console.WriteLine("Press Ctrl+C to exit the prompt.");
+
+            while (true) {
+                Console.Write("> ");
+                interpreter.interprete(Console.In.ReadLine());
+            }
         }
     }
 }
