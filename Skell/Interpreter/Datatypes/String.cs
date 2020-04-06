95da96d7e0dfc252bf53ca40d58fc3428b888b4c
Pulak Kumar Das <pulak1901@gmail.com>
2020-04-06 17:00:37 +0530

    Implement equality checking for String

--- Skell/Interpreter/Datatypes/String.cs
+++ Skell/Interpreter/Datatypes/String.cs
@@ -4,7 +4,7 @@ namespace Skell.Data
 {
     class String : SkellData
     {
-        string contents;
+        public readonly string contents;
 
         public String(string token)
         {
@@ -18,6 +18,26 @@ namespace Skell.Data
             s.Append(contents);
             s.Append("\"");
             return s.ToString();
+        }
+
+        public static bool operator ==(String s1, String s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        public static bool operator !=(String s1, String s2)
+        {
+            return !s1.Equals(s2);
+        }
+
+        public override int GetHashCode() => contents.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is String s) {
+                return contents == s.contents;
+            }
+            throw new System.NotImplementedException();
         }
     }
 }
