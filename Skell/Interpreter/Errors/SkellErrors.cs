9c5ed00b4f554e0303f51c5140c4b12cc1ada433
Pulak Kumar Das <pulak1901@gmail.com>
2020-04-06 17:41:36 +0530

    Added constructors and access modifier to NativeParseError

--- Skell/Interpreter/Errors/SkellErrors.cs
+++ Skell/Interpreter/Errors/SkellErrors.cs
@@ -2,11 +2,21 @@ using Serilog;
 
 namespace Skell.Error
 {
-    class NativeParseError : System.Exception
+    internal class NativeParseError : System.Exception
     {
         public NativeParseError(string token)
         {
             Log.Warning($"Failed to parse \"{token}\" to native type");
+        }
+
+        public NativeParseError() : base()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NativeParseError(string message, System.Exception innerException) : base(message, innerException)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
