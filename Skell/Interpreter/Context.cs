4d1e3b867e8f0595e7f6e60e305b73aa7fc4225a
Pulak Kumar Das <pulak1901@gmail.com>
2020-04-06 23:10:53 +0530

    Log every variable access in verbose mode and every assignment/deletion in debug mode

--- Skell/Interpreter/Context.cs
+++ Skell/Interpreter/Context.cs
@@ -1,28 +1,40 @@
 using System.Collections.Generic;
+using Serilog;
 
 namespace Skell.Interpreter
 {
     internal class Context
     {
+        private readonly ILogger logger;
         private readonly Dictionary<string, Skell.Types.ISkellType> mem;
 
         public Context()
         {
+            logger = Log.ForContext<Context>();
             mem = new Dictionary<string, Skell.Types.ISkellType>();
         }
 
         public bool Exists(string name) => mem.ContainsKey(name);
 
-        public void Set(string name, Skell.Types.ISkellType data) => mem[name] = data;
+        public void Set(string name, Skell.Types.ISkellType data)
+        {
+            mem[name] = data;
+            logger.Debug($"{name} is now {data}");
+        }
 
         public Skell.Types.ISkellType Get(string name)
         {
             if (!Exists(name)) {
                 throw new System.NotImplementedException();
             }
+            logger.Verbose($"Accessing {name}");
             return mem[name];
         }
 
-        public void Delete(string name) => mem.Remove(name);
+        public void Delete(string name) 
+        {
+            logger.Debug($"{name} was {mem[name]}, now removed");
+            mem.Remove(name);
+        }
     }
 }
